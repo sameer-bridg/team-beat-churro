ffcea3ebf9951d4ceb13c65072901784ac2b9e84
Ron Rojany <ron@ronrojany.com>
2015-02-25 16:35:16 -0800

    hey sameer here is my latest

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -34,8 +34,17 @@ angular.module('teamChurroApp')
       return new Array(n);
     };
 
-    $scope.grade = 'B';
-    
+    $scope.grade = 'B+';
+    $scope.grade_A_minus_text = 'You’re amongst the top 10% of brands when it comes to data-driven marketing.';
+    $scope.grade_B_plus_text = 'You’ve got great data coverage on your customers - but is your data actionable?';
+    $scope.grade_B_text = 'Better than B-, not as good as B+. Are you leveraging all of your data?';
+    $scope.grade_B_minus_text = 'You have most of your bases covered but there are still some obvious gaps.';
+    $scope.grade_C_plus_text = 'You’ve got some cool marketing tools - but there’s still a lot you’re missing out on.';
+    $scope.grade_C_text = 'Your technological capabilities are sitting around the industry average.';
+    $scope.grade_C_minus_text = 'You’ve covered the basics, but you’re missing quite a few important pieces.';
+    $scope.grade_D_text = 'You’re missing a major piece of marketing infrastructure.';
+    $scope.grade_F_text = 'Are you sure you’re not using any other vendors?';
+
 
     (function () {
       processSelections($scope.defaultSelections, $scope.categories);
